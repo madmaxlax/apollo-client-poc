e20cb5b43e299f2bc080ac21711e1607363fd291
Max Struever <struever.max@bcgplatinion.com>
2022-04-11 18:12:54 -0500

    working

--- src/pages/QueryPage/index.tsx
+++ src/pages/QueryPage/index.tsx
@@ -41,6 +41,16 @@ export const QueryPage = () => {
   if (error) return <p>Error :({error.message})</p>;
   return (
     <>
+      <Typography variant="h3">
+        Example with Apollo{' '}
+        <a
+          target="_blank"
+          href="https://www.apollographql.com/docs/react/local-state/reactive-variables/"
+          rel="noreferrer"
+        >
+          Reactive Variables
+        </a>
+      </Typography>
       <Typography>Fav Color: {mySettingsData.favoriteColor}</Typography>
       <Typography>Other fav Color: {otherSettingsData.favoriteColor}</Typography>
       <Button
@@ -50,10 +60,10 @@ export const QueryPage = () => {
           // initialUserSettingsVar({ ...initialUserSettingsVar(), favoriteColor: 'blue' });
         }}
       >
-        Add Color
+        Set Color to ReactiveVar
       </Button>
       <Divider />
-      <Typography variant="h2">Returned from </Typography>
+      <Typography variant="h3">Returned from GraphQL</Typography>
       {data?.characters?.results.map((character: Character, index: number) => (
         <div key={index}>
           <p>
